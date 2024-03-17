1df07cecd9ddb6f1b4959d686b7a6b89f48f79ff
신동규 <donggyu9410@gmail.com>
2024-03-17 18:11:13 +0900

    feat: hide scroll

--- dg-muscle-ios/view/history/HistoryFormView.swift
+++ dg-muscle-ios/view/history/HistoryFormView.swift
@@ -86,6 +86,7 @@ struct HistoryFormView: View {
                     }
                 }
             }
+            .scrollIndicators(.hidden)
             
             if saveButtonDisabled == false {
                 Button {
